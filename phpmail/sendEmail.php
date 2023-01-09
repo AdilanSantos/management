e16b7819affd4b2fef4e8e02a45c4381af20d9b2
Adilan Araujo <41523586+AdilanSantos@users.noreply.github.com>
2023-01-09 00:27:41 -0300

    Update sendEmail.php

--- phpmail/sendEmail.php
+++ phpmail/sendEmail.php
@@ -18,7 +18,7 @@ function sendEmail($sEmail, $sName, $sha1Token){
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'notifymanagementapp@gmail.com';                     //SMTP username
-        $mail->Password   = 'vsqunzgcdhlywwoq';                               //SMTP password
+        $mail->Password   = '';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
